e210797c6895f17223e07ba271c3f2542f43566a
Bayo17 <bayoumyhazem@itis-molinari.eu>
2025-02-17 13:54:39 +0100

    test

--- componenti/navigator.js
+++ componenti/navigator.js
@@ -26,4 +26,3 @@ const createNavigator = (parentElement) => {
     render();
   };
 export {createNavigator};
-  
--- public/script.js
+++ public/script.js
@@ -1,28 +1,8 @@
 import { createNavigator } from "/componenti/navigator.js";
-const prevBtn = document.getElementById('prev-btn');
-const nextBtn = document.getElementById('next-btn');
-const carouselImage = document.getElementById('carousel-image');
-const images = [
-    '/assets/immagine1.jpeg',
-    '/assets/immagine2.jpeg',
-    '/assets/immagine3.jpeg',
-    '/assets/immagine4.jpeg',
-    '/assets/immagine5.jpeg'
-];
-let currentIndex = 0;
-function updateCarousel() {
-    carouselImage.src = images[currentIndex];
-}
+import { add_images } from "../componenti/add_images.js";
+const login_btn = document.getElementById("login_btn");
 
-function showPrevImage() {
-    currentIndex = (currentIndex - 1 + images.length) % images.length;
-    updateCarousel();
-}
-function showNextImage() {
-    currentIndex = (currentIndex + 1) % images.length;
-    updateCarousel();
-}
-prevBtn.onclick = () => {showPrevImage()};
-nextBtn.onclick = () => {showNextImage()};
-updateCarousel();
-const navigator = createNavigator(document.querySelector("#spa-container"));
+login_btn.onclick = () => {
+    document.getElementById("login").style.display= "block";
+};
+
